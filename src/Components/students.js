d19edaa2bb566a966f219c19d14b424889fcf4ee
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-04 15:03:09 -0500

    search bar added anchored

--- src/Components/students.js
+++ src/Components/students.js
@@ -3,14 +3,13 @@ import { getStudents } from '../Hooks/students';
 
 export default function StudentsDisplay(props) {
   const [students, setStudents] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
 
   // Load students
   useEffect(() => {
     async function getData() {
       const studentData = await getStudents();
-      console.log('STUDENT DATA ASYNC: ', studentData);
       setStudents(studentData);
-      console.log(students);
     }
 
     getData();
@@ -21,42 +20,65 @@ export default function StudentsDisplay(props) {
       {!students ? (
         'Loading'
       ) : (
-        <div className="allStudentsContainer">
-          {students.map((student) => {
-            return (
-              <div className="studentContainer">
-                <div className="studentImageDiv">
-                  <img
-                    className="studentImage"
-                    src={student.pic}
-                    alt="studentImage"
-                  />
-                </div>
-                <div>
-                  <p>
-                    <h1 className="studentName">
-                      {student.firstName + ' ' + student.lastName}
-                    </h1>
-                  </p>
-                  <div className="studentInfo">
-                    <span>Email: {student.email}</span>
-                    <span>Company: {student.company}</span>
-                    <span>Skill: {student.skill}</span>
-                    <span>
-                      {'Average: ' +
-                        student.grades.reduce(function (sum, value) {
-                          sum = parseInt(sum);
-                          value = parseInt(value);
-                          return sum + value;
-                        }, 0) /
-                          student.grades.length +
-                        '%'}
-                    </span>
+        <div className="mainContainer">
+          <div className="searchBarContainer">
+            <input
+              type="text"
+              name="name"
+              placeholder="Search by name"
+              className="searchBar"
+              onChange={(event) => setSearchInput(event.target.value)}
+            />
+          </div>
+          <div className="allStudentsContainer">
+            {students
+              .filter((student) => {
+                const studentName = student.firstName + ' ' + student.lastName;
+
+                if (searchInput === '') {
+                  return student;
+                } else {
+                  if (studentName.includes(searchInput)) {
+                    return student;
+                  }
+                }
+              })
+              .map((student) => {
+                return (
+                  <div className="studentContainer">
+                    <div className="studentImageDiv">
+                      <img
+                        className="studentImage"
+                        src={student.pic}
+                        alt="studentImage"
+                      />
+                    </div>
+                    <div>
+                      <p>
+                        <h1 className="studentName">
+                          {student.firstName + ' ' + student.lastName}
+                        </h1>
+                      </p>
+                      <div className="studentInfo">
+                        <span>Email: {student.email}</span>
+                        <span>Company: {student.company}</span>
+                        <span>Skill: {student.skill}</span>
+                        <span>
+                          {'Average: ' +
+                            student.grades.reduce(function (sum, value) {
+                              sum = parseInt(sum);
+                              value = parseInt(value);
+                              return sum + value;
+                            }, 0) /
+                              student.grades.length +
+                            '%'}
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              })}
+          </div>
         </div>
       )}
     </div>
