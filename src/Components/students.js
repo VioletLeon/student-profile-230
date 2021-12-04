f575b700d3c6bf6c99c4d0c63437b4866212c7c8
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-04 16:29:42 -0500

    part 4 completed

--- src/Components/students.js
+++ src/Components/students.js
@@ -3,6 +3,7 @@ import { getStudents } from '../Hooks/students';
 
 export default function StudentsDisplay(props) {
   const [students, setStudents] = useState([]);
+  const [activeStudents, setActiveStudent] = useState([]);
   const [searchInput, setSearchInput] = useState('');
 
   // Load students
@@ -44,37 +45,81 @@ export default function StudentsDisplay(props) {
                 }
               })
               .map((student) => {
+                student['active'] = false;
+
                 return (
-                  <div className="studentContainer">
-                    <div className="studentImageDiv">
-                      <img
-                        className="studentImage"
-                        src={student.pic}
-                        alt="studentImage"
-                      />
-                    </div>
-                    <div>
-                      <p>
-                        <h1 className="studentName">
-                          {student.firstName + ' ' + student.lastName}
-                        </h1>
-                      </p>
-                      <div className="studentInfo">
-                        <span>Email: {student.email}</span>
-                        <span>Company: {student.company}</span>
-                        <span>Skill: {student.skill}</span>
-                        <span>
-                          {'Average: ' +
-                            student.grades.reduce(function (sum, value) {
-                              sum = parseInt(sum);
-                              value = parseInt(value);
-                              return sum + value;
-                            }, 0) /
-                              student.grades.length +
-                            '%'}
-                        </span>
+                  <div className="outerStudentContainer">
+                    <div className="studentContainer">
+                      <div className="studentImageDiv">
+                        <img
+                          className="studentImage"
+                          src={student.pic}
+                          alt="studentImage"
+                        />
+                      </div>
+                      <div>
+                        <p>
+                          <h1 className="studentName">
+                            {student.firstName + ' ' + student.lastName}
+                          </h1>
+                        </p>
+                        <div className="studentInfo">
+                          <span>Email: {student.email}</span>
+                          <span>Company: {student.company}</span>
+                          <span>Skill: {student.skill}</span>
+                          <span>
+                            {'Average: ' +
+                              student.grades.reduce(function (sum, value) {
+                                sum = parseInt(sum);
+                                value = parseInt(value);
+                                return sum + value;
+                              }, 0) /
+                                student.grades.length +
+                              '%'}
+                          </span>
+                        </div>
+                      </div>
+                      <div className="buttonExpand">
+                        {!activeStudents.includes(student.id) ? (
+                          <button
+                            className="btn"
+                            onClick={() => {
+                              setActiveStudent([...activeStudents, student.id]);
+                            }}
+                          >
+                            <i class="fas fa-plus"></i>
+                          </button>
+                        ) : (
+                          <button
+                            className="btn"
+                            onClick={() => {
+                              setActiveStudent(
+                                activeStudents.filter(
+                                  (studentid) => studentid !== student.id
+                                )
+                              );
+                            }}
+                          >
+                            <i class="fas fa-minus"></i>
+                          </button>
+                        )}
                       </div>
                     </div>
+                    {activeStudents.includes(student.id) ? (
+                      <div className="gradesContainer">
+                        {student.grades.map((grade, index) => {
+                          return (
+                            <span>
+                              Test {index + 1}:{' '}
+                              <span className="gradeText">{grade} %</span>
+                            </span>
+                          );
+                        })}
+                      </div>
+                    ) : (
+                      <div />
+                    )}
+                    <hr />
                   </div>
                 );
               })}
