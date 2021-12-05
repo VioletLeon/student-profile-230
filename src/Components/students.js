eb3edabef1fa5de8ac2ba5a87a87248c16cb6bcd
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-04 19:12:13 -0500

    finished all parts

--- src/Components/students.js
+++ src/Components/students.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { act } from 'react-dom/test-utils';
 import { getStudents } from '../Hooks/students';
 
 export default function StudentsDisplay(props) {
   const [students, setStudents] = useState([]);
   const [activeStudents, setActiveStudent] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+  const [tagInput, setTagInput] = useState('');
 
   // Load students
   useEffect(() => {
@@ -12,7 +14,6 @@ export default function StudentsDisplay(props) {
       const studentData = await getStudents();
       setStudents(studentData);
     }
-
     getData();
   }, []);
 
@@ -28,27 +29,55 @@ export default function StudentsDisplay(props) {
               name="name"
               placeholder="Search by name"
               className="searchBar"
-              onChange={(event) => setSearchInput(event.target.value)}
+              onChange={(event) =>
+                setSearchInput(event.target.value.toLowerCase())
+              }
             />
+            <div className="searchBarContainer">
+              <input
+                type="text"
+                name="name"
+                placeholder="Search by tag"
+                className="searchBar"
+                onChange={(event) =>
+                  setTagInput(event.target.value.toLowerCase())
+                }
+              />
+            </div>
           </div>
           <div className="allStudentsContainer">
             {students
               .filter((student) => {
                 const studentName = student.firstName + ' ' + student.lastName;
+                const lowerCase = studentName.toLowerCase();
 
                 if (searchInput === '') {
                   return student;
                 } else {
-                  if (studentName.includes(searchInput)) {
+                  if (lowerCase.includes(searchInput)) {
+                    return student;
+                  }
+                }
+              })
+              .filter((student) => {
+                const tags = student.tags;
+                if (tagInput === '') {
+                  return student;
+                } else {
+                  if (
+                    tags.some((value) => value.toLowerCase().includes(tagInput))
+                  ) {
                     return student;
                   }
                 }
               })
               .map((student) => {
-                student['active'] = false;
-
+                if (!student.active && !student.tags) {
+                  student['active'] = false;
+                  student['tags'] = [];
+                }
                 return (
-                  <div className="outerStudentContainer">
+                  <div key={student.id} className="outerStudentContainer">
                     <div className="studentContainer">
                       <div className="studentImageDiv">
                         <img
@@ -58,11 +87,10 @@ export default function StudentsDisplay(props) {
                         />
                       </div>
                       <div>
-                        <p>
-                          <h1 className="studentName">
-                            {student.firstName + ' ' + student.lastName}
-                          </h1>
-                        </p>
+                        <h1 className="studentName">
+                          {student.firstName + ' ' + student.lastName}
+                        </h1>
+
                         <div className="studentInfo">
                           <span>Email: {student.email}</span>
                           <span>Company: {student.company}</span>
@@ -77,6 +105,37 @@ export default function StudentsDisplay(props) {
                                 student.grades.length +
                               '%'}
                           </span>
+
+                          {student.tags < 1 ? (
+                            ''
+                          ) : (
+                            <div className="tagsMain">
+                              {student['tags'].map((tag, index) => {
+                                return (
+                                  <div className="tagContainer" key={index}>
+                                    <span className="tagText"> {tag} </span>
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          )}
+
+                          <form
+                            onSubmit={(event) => {
+                              event.preventDefault();
+                              student['tags'].push(event.target[0].value);
+                              const copy = [...students];
+                              setStudents(copy);
+                              event.target[0].value = '';
+                            }}
+                          >
+                            <input
+                              type="text"
+                              tag="tag"
+                              placeholder="Add a tag"
+                              className="tagInput"
+                            />
+                          </form>
                         </div>
                       </div>
                       <div className="buttonExpand">
@@ -87,7 +146,7 @@ export default function StudentsDisplay(props) {
                               setActiveStudent([...activeStudents, student.id]);
                             }}
                           >
-                            <i class="fas fa-plus"></i>
+                            <i className="fas fa-plus"></i>
                           </button>
                         ) : (
                           <button
@@ -100,7 +159,7 @@ export default function StudentsDisplay(props) {
                               );
                             }}
                           >
-                            <i class="fas fa-minus"></i>
+                            <i className="fas fa-minus"></i>
                           </button>
                         )}
                       </div>
